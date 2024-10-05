3320a76cf28ef986de34c92b3a915e3fa7a456f9
Ayrik Nabirahni <ayrytube@gmail.com>
2024-10-05 09:49:18 -0700

    V1.0.0-2.0.0-1.0.0 - decryption initial implementation
    KNOWN ISSUES
    - no frontend currently

--- clipperDecryption/decryption.py
+++ clipperDecryption/decryption.py
@@ -1,4 +1,27 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from clipperEncryption.ifArray import *
 
 
 def clipperDecryption(word, seed):
-    
+    seed_index_list = []
+    letter_index_list = []
+    decrypted_word_list = []
+    for char in word:
+        letter_index_list.append(character_window_index_str[char])
+    print(letter_index_list)
+    for letter_index in letter_index_list:
+        for seed_index in range(len(seed)):
+            print(letter_index, seed[seed_index])
+            if letter_index == seed[seed_index]: # if the alphabetical index of the letter matches the seed, take the index of the seed
+                seed_index_list.append(seed_index)
+                print('match found')
+                break
+        
+    for num in seed_index_list:
+        decrypted_word_list.append(character_window_inverse_index_int[num])
+    return ''.join(decrypted_word_list)
+
+
+print(clipperDecryption('TYQKGSTGAAE', ['21', '07', '20', '14', '03', '04', '11', '17', '18', '25', '08', '13', '01', '15', '02', '16', '24', '26', '12', '05', '19', '23', '06', '09', '10', '22']))
--- clipperEncryption/encryption.py
+++ clipperEncryption/encryption.py
@@ -3,11 +3,12 @@ from ifArray import *
 def clipperEncryption(word, seed):
     seeded_list = []
     encrypted_list = []
-    word_list = [char.upper() for char in word]
+    word_list = [char.upper() for char in word] # converts word_list to uppercase
     for char in word_list:
-        window = character_window_index[char]
-        seeded_list.append(seed[window])
+        window = character_window_index[char] # converts each 'letter' to its alphabetical index (A = 1, B = 2, etc)
+        seeded_list.append(seed[window]) 
     for char in seeded_list:
-        encrypted_list.append(character_window_inverse_index[char])
+        encrypted_list.append(character_window_inverse_index[char]) # converts the 1-26 seed back into a letter
     return ''.join(encrypted_list)
-print(clipperEncryption('parser', ['21', '07', '20', '14', '03', '04', '11', '17', '18', '25', '08', '13', '01', '15', '02', '16', '24', '26', '12', '05', '19', '23', '06', '09', '10', '22']))
+
+print(clipperEncryption('bigfatballs', ['21', '07', '20', '14', '03', '04', '11', '17', '18', '25', '08', '13', '01', '15', '02', '16', '24', '26', '12', '05', '19', '23', '06', '09', '10', '22']))
