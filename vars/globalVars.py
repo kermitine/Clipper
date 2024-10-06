102c6d3f23bd989ff3b7e0a9836bd7a1e372fbe8
Ayrik Nabirahni <ayrytube@gmail.com>
2024-10-05 20:50:53 -0700

    V1.2.0-1.1.1-2.1.1-2.2.1 -fixed a major bug which caused 'Z' to crash the program

--- vars/globalVars.py
+++ vars/globalVars.py
@@ -1,6 +1,6 @@
 version_Clipper = '1.2.0'
-version_Decryption = '1.1.0'
+version_Decryption = '1.1.1'
 version_seedGen = '2.1.1'
-version_Encryption = '2.2.0'
+version_Encryption = '2.2.1'
 
 gap_time = 0.3
